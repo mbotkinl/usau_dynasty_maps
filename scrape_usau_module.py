883512a6c77dd49f0282a1faef09a3aacc712609
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2019-09-16 23:07:43 -0700

    Functionalize

--- scrape_usau_module.py
+++ scrape_usau_module.py
@@ -1,58 +1,15 @@
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup
+from scrape_utils import get_data_for_year
 
-# url = 'https://play.usaultimate.org/events/USA-Ultimate-National-Championships-2015/schedule/Men/Club-Men/'
-# url = 'https://www.usaultimate.org/archives/2018_club.aspx'
-url = 'https://www.usaultimate.org/archives/2001_club.aspx'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-# print(soup.prettify())
+# start_year = 1979
+# end_year = 2018
+start_year = 2001
+end_year = 2002
 
-
-def parse_div(divs):
-    # TODO: do this
-    return ['Mixed', 'Open', 'Womens']
-
-
-def clean_data(df):
-    df = df.copy()
-    df.Standing = df.Standing.str.replace('T', '')
-    df.dropna(inplace=True)
-    df.reset_index(drop=True, inplace=True)
-    return df
-
-
-divisions = []
-for d in soup.find_all('h3'):
-    if d.a:
-        for a in d.find_all('a'):
-            div_name = a.attrs.get('name', None)
-            if div_name and div_name.startswith('nats'):
-                divisions.append(d.span.text)
-
-divisions = parse_div(divisions)
-
-
-tables = soup.find_all('table', {'class': 'tablesorter'})
-
-year_df = pd.DataFrame()
-for i, table in enumerate(tables):
-    headings = []
-    for h in table.find_all('th'):
-        headings.append(h.text)
-    l = []
-    for tr in table.find_all('tr'):
-        td = tr.find_all('td')
-        row = [tr.text for tr in td]
-        l.append(row)
-    df_table = pd.DataFrame(l, columns=headings)
-    df_table['division'] = divisions[i]
-    year_df = pd.concat([year_df, df_table], sort=False)
-
-
-year_df = clean_data(year_df)
-
+all_data = pd.DataFrame()
+for year in range(start_year, end_year+1):
+    data = get_data_for_year(year)
+    all_data = pd.concat([all_data, data], sort=False)
 
 
 # soup.find_all('h4')
---
+++ scrape_utils.py
@@ -0,0 +1,53 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+def parse_div(divs):
+    # TODO: do this
+    return ['Mixed', 'Open', 'Womens']
+
+
+def clean_data(df):
+    df = df.copy()
+    df.Standing = df.Standing.str.replace('T', '')
+    df.dropna(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+def get_data_for_year(year):
+    # url = 'https://play.usaultimate.org/events/USA-Ultimate-National-Championships-2015/schedule/Men/Club-Men/'
+    # url = 'https://www.usaultimate.org/archives/2018_club.aspx'
+    # url = 'https://www.usaultimate.org/archives/2001_club.aspx'
+    url = f'https://www.usaultimate.org/archives/{year}_club.aspx'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    divisions = []
+    for d in soup.find_all('h3'):
+        if d.a:
+            for a in d.find_all('a'):
+                div_name = a.attrs.get('name', None)
+                if div_name and div_name.startswith('nats'):
+                    divisions.append(d.span.text)
+
+    divisions = parse_div(divisions)
+
+    tables = soup.find_all('table', {'class': 'tablesorter'})
+
+    year_df = pd.DataFrame()
+    for i, table in enumerate(tables):
+        headings = []
+        for h in table.find_all('th'):
+            headings.append(h.text)
+        table_entries = []
+        for tr in table.find_all('tr'):
+            td = tr.find_all('td')
+            row = [tr.text for tr in td]
+            table_entries.append(row)
+        df_table = pd.DataFrame(table_entries, columns=headings)
+        df_table['division'] = divisions[i]
+        year_df = pd.concat([year_df, df_table], sort=False)
+
+    year_df = clean_data(year_df)
+    return year_df
