202a4189a3eefddbf5b5f607cf26d342e2e08936
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2019-09-18 21:03:54 -0700

    Working data collection for all years

--- scrape_usau_module.py
+++ scrape_usau_module.py
@@ -1,16 +1,15 @@
 import pandas as pd
 from scrape_utils import get_data_for_year
 
-# start_year = 1979
-# end_year = 2018
-start_year = 2001
-end_year = 2002
+start_year = 1979
+end_year = 2018
+# start_year = 2000
+# end_year = 2002
 
 all_data = pd.DataFrame()
 for year in range(start_year, end_year+1):
+    print(year)
     data = get_data_for_year(year)
     all_data = pd.concat([all_data, data], sort=False)
 
-
-# soup.find_all('h4')
-# mydivs = soup.findAll("div", {"class": "top_area winner"})
+all_data.to_csv('./data/national_data.csv', index=False)
--- scrape_utils.py
+++ scrape_utils.py
@@ -2,10 +2,17 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+DIV_NAMES = [' MIXED ', ' MENS ', 'CO-ED ', ' OPEN ', ' WOMENS ']
 
-def parse_div(divs):
-    # TODO: do this
-    return ['Mixed', 'Open', 'Womens']
+
+def parse_div(divisions):
+    # todo: do this better
+    new_divs = []
+    for div in divisions:
+        for name in DIV_NAMES:
+            if name in div:
+                new_divs.append(name.strip())
+    return new_divs
 
 
 def clean_data(df):
@@ -17,9 +24,6 @@ def clean_data(df):
 
 
 def get_data_for_year(year):
-    # url = 'https://play.usaultimate.org/events/USA-Ultimate-National-Championships-2015/schedule/Men/Club-Men/'
-    # url = 'https://www.usaultimate.org/archives/2018_club.aspx'
-    # url = 'https://www.usaultimate.org/archives/2001_club.aspx'
     url = f'https://www.usaultimate.org/archives/{year}_club.aspx'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -28,15 +32,19 @@ def get_data_for_year(year):
         if d.a:
             for a in d.find_all('a'):
                 div_name = a.attrs.get('name', None)
-                if div_name and div_name.startswith('nats'):
-                    divisions.append(d.span.text)
+                if div_name and div_name.startswith('nats_'):
+                    divisions.append(d.span.text.upper().replace("'", ""))
 
+    print(divisions)
     divisions = parse_div(divisions)
+    print(divisions)
 
     tables = soup.find_all('table', {'class': 'tablesorter'})
-
+    # assumes nationals tables are first
+    tables = tables[0:len(divisions)]
     year_df = pd.DataFrame()
     for i, table in enumerate(tables):
+        print(i)
         headings = []
         for h in table.find_all('th'):
             headings.append(h.text)
