f1df5290161f6464828f4ba8c9a90cf6ea7aff0e
Micah Botkin-Levy <micah@eiqmobility.com>
2019-10-06 10:22:34 -0700

    Add appearances hist

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from visualize_usau_module import ranking_data, DIVISIONS, REGIONS
+from visualize_usau_module import ranking_data, appearance_hist, DIVISIONS, REGIONS
 
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 fig_rankings = ranking_data(division='MENS')
+fig_hist = appearance_hist(division='MENS')
 
 app.layout = html.Div(children=[
     html.H1(children='USAU Club Visualization'),
@@ -39,15 +40,18 @@ app.layout = html.Div(children=[
         ])
     ]),
     html.Div([dcc.Graph(id='rankings_graph', figure=fig_rankings)]),
+    html.Div([dcc.Graph(id='appearance_graph', figure=fig_hist)])
 ])
 
 
-@app.callback(
+@app.callback([
     Output('rankings_graph', 'figure'),
+    Output('appearance_graph', 'figure')],
     [Input('division_dropdown', 'value'), Input('region_dropdown', 'value')])
 def update_figure(division, region):
-    new_fig = ranking_data(division, region)
-    return new_fig
+    new_ranking = ranking_data(division, region)
+    new_hist = appearance_hist(division, region)
+    return new_ranking, new_hist
 
 
 if __name__ == '__main__':
--- visualize_usau_module.py
+++ visualize_usau_module.py
@@ -6,6 +6,8 @@ data = pd.read_csv('./data/national_data.csv')
 DIVISIONS = data.division.unique()
 REGIONS = data.Region.unique()
 
+TOP_NUM = 4
+
 
 def ranking_data(division, region='all'):
     if region == 'all':
@@ -18,7 +20,24 @@ def ranking_data(division, region='all'):
                             name=t) for t in div_df.Team.unique()]
 
     layout = {'title': 'Club Nationals Placement',
-              'yaxis': {'autorange': 'reversed', 'zeroline': False,
+              'xaxis': {'title': 'Year'},
+              'yaxis': {'autorange': 'reversed', 'zeroline': False, 'title': 'Nationals Placement',
                         'range': [1, max(div_df['Standing'])]}}  # todo: fix range
 
+    return dict(data=plot_data, layout=layout)
+
+
+def appearance_hist(division, region='all'):
+    # todo: sync colors
+    if region == 'all':
+        div_df = data[(data.division == division)].copy()
+    else:
+        div_df = data[(data.division == division) & (data.Region == region)].copy()
+    appearances = div_df.Team.value_counts()
+    hist_data = div_df[div_df.Team.isin(appearances[appearances > TOP_NUM].index.values)]
+    plot_data = [go.Histogram(x=hist_data.Team)]
+    layout = {'title': 'Most Nationals Appearances',
+              'yaxis': {'title': 'Number of Nationals Appearance'},
+              'xaxis': {'title': 'Team',
+                        'categoryorder': 'total descending'}}
     return dict(data=plot_data, layout=layout)
