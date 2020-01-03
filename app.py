ac88861e653cf06a97482dd75ef8052022c70451
Micah Botkin-Levy <micah@eiqmobility.com>
2020-01-03 08:41:32 -0800

    Transparent background and margin improvements

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from visualize_usau_module import ranking_data, spirit_correlation, \
     COMP_DIVISIONS, get_divisions, get_regions, table_data
 
 from dash_constants import BACKGROUND_COLOR_DARK, BACKGROUND_COLOR_LIGHT, \
-    PLOT_BACKGROUND_COLOR, HEADER_2_SIZE
+    PLOT_BACKGROUND_COLOR, HEADER_2_SIZE, BACKGROUND_LIGHT_RGB, BACKGROUND_ALPHA
 
 style = {'backgroundColor': BACKGROUND_COLOR_LIGHT, 'font-family': 'Arial'}
 
@@ -46,25 +46,28 @@ app.layout = html.Div(style=style, children=[
                                        }),
 
     # intro section
-    html.Div([dcc.Markdown('''
-    ## About the project
-    As a frisbee player, I was interested to see the rise and fall of teams in each region over the years.
-    To build this I scraped data from the USAU archives of nationals.
-
-
-    ## How to use
-    * Use the Data Subsetting section to pick a division, a sub-division, and a region
-    * Use checkboxes in table to pick which teams to show
-    * The graphs will update automatically and can be hovered over for information
-
-    ### Notes
-    * Divisions are named based on current USAU naming except in the case of college where there are separate
-    sub divisions for before and after the DI/DIII separaration
-    * Due to regional boundary redrawing, the regionals of teams change over the years
-
-    Questions/comments? Feel free to contact on LinkedIn or GitHub with the links at the bottom.
-    ''', style={'backgroundColor': PLOT_BACKGROUND_COLOR,
-                'margin-left': '60px', 'margin-right': '60px', 'padding': 2})],
+    html.Div([
+        html.Div([
+            dcc.Markdown('''
+                ## About the project
+                As a frisbee player, I was interested to see the rise and fall of teams in each region over the years.
+                To build this I scraped data from the USAU archives of nationals.
+            
+            
+                ## How to use
+                * Use the Data Subsetting section to pick a division, a sub-division, and a region
+                * Use checkboxes in table to pick which teams to show
+                * The graphs will update automatically and can be hovered over for information
+            
+                ### Notes
+                * Divisions are named based on current USAU naming except in the case of college where there are separate
+                sub divisions for before and after the DI/DIII separation
+                * Due to regional boundary redrawing, the regionals of teams change over the years
+            
+                Questions/comments? Feel free to contact on LinkedIn or GitHub with the links at the bottom.
+                ''', style={'margin-left': '20px', 'margin-right': '20px'})],
+            style={'backgroundColor': f'rgba{BACKGROUND_LIGHT_RGB + (BACKGROUND_ALPHA,)}',
+                   'margin-left': '40px', 'margin-right': '40px', 'padding': 2, 'vertical-align': 'middle'})],
              style={
                  # 'backgroundColor': PLOT_BACKGROUND_COLOR,
                       'background-image': f'url({TURF_LINE_IMAGE})',
@@ -116,7 +119,8 @@ app.layout = html.Div(style=style, children=[
                         value='all')
                 ])
             ])
-        ], fluid=True, style={'padding': 40})
+        ], fluid=True, style={'padding': 20,
+                              'backgroundColor': f'rgba{BACKGROUND_LIGHT_RGB + (BACKGROUND_ALPHA,)}'})
     ], style={'padding': 10, 'background-image': f'url({TURF_LINE_IMAGE})',
               'min-height': '250px',
               'background-attachment': 'fixed',
